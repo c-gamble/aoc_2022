67279c13c625ee6c1f2ecf19f6d29de4980d7ffd
Cooper Gamble <gamble.cooper23@gmail.com>
2022-12-05 23:08:31 -0600

    day6

---
+++ day6/p2.py
@@ -0,0 +1,23 @@
+string = ""
+with open('./in.txt') as file:
+    for line in file:
+        line = line.split('\n')[0]
+        string += line
+
+def occur(s, e):
+    r = 0
+    for c in s:
+        if e == c: r += 1
+    return r
+def check_repeating(s):
+    for c in s:
+        if occur(s, c) > 1: return True
+    return False
+#window size 4
+ans = ''
+for i in range(len(string)):
+    curr_window = string[i:i+14]
+    if check_repeating(curr_window): continue
+    else:
+        print(i+14)
+        break
